2ee78ffac928d7fb78607ec713172718a4a2a56f
Haegnim <haegnim@gmail.com>
2021-06-01 15:40:15 +0900

    0601 .intro 슬라이드 & 반응형 디자인

--- js/index.js
+++ js/index.js
@@ -1,6 +1,16 @@
+//Contents Text
 const conTxt = document.querySelectorAll('.con-txt p a');
 
 conTxt.forEach(element => {
     const cutTxt = element.textContent.slice(0,10) + "...";
     element.textContent = cutTxt;
 });
+
+//Mobile Menu Activate
+const mobileMenu = document.querySelector('.mobile-menu');
+
+mobileMenu.onclick = () => {
+    mobileMenu.classList.toggle('active');
+
+}
+
